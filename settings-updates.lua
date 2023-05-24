0c7f5aa79ae18005af27acade47f23ce23f09e53
Andrew Hoffman <andrewglennhoffman@gmail.com>
2023-05-23 21:26:57 -0500

    Crash fix when AAI Industries was disabled.

--- settings-updates.lua
+++ settings-updates.lua
@@ -6,5 +6,7 @@ data.raw["double-setting"]["rso-resource-chance"].default_value = 0.5
 data.raw["string-setting"]["oil_richness"].default_value = "very-good"
 data.raw["bool-setting"]["start-with-random-color"].default_value = true
 data.raw["bool-setting"]["aai-remove-ai-on-mine"].default_value = true
-data.raw["bool-setting"]["aai-remove-ai-on-mine"].default_value = true
-data.raw["bool-setting"]["aai-fuel-processor"].default_value = false
+
+if(data.raw["bool-setting"]["aai-fuel-processor"]) then
+	data.raw["bool-setting"]["aai-fuel-processor"].default_value = false
+end
